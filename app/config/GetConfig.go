e8485bc529be8a044d760bc29d1ae4d5688cbd03
Antony Grachev <personalsmile69@gmail.com>
2024-07-14 19:50:21 +0300

    return go-file from config folder

---
+++ app/config/GetConfig.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Command struct {
+		Cmnd         string `yaml:"exec_value"`
+		CmdShellFlag bool   `yaml:"cmd_shell_flag"`
+		CmdShellExec string `yaml:"cmd_shell_exec"`
+	} `yaml:"command"`
+	Pswrd string `yaml:"password"`
+	Days  []int  `yaml:"days"`
+	Hash  string `yaml:"hash"`
+}
+
+func LoadConfig() Config {
+	// Create new struct
+	var config Config
+
+	// Get executable path
+	execPath, err := os.Executable()
+	if err != nil {
+		log.Fatalf("ошибка получения пути исполняемого файла: %v", err)
+		return config
+	}
+
+	// Get executable dir
+	execDir := filepath.Dir(execPath)
+
+	// Set config path
+	filename := filepath.Join(execDir, "config", "config.yaml")
+
+	// Check if file exists
+	_, err = os.Stat(filename)
+	if err != nil {
+		log.Fatalf("файл конфигурации '%s' не существует: %v", filename, err)
+		return config
+	}
+
+	// Check if file is readable
+	_, err = os.Open(filename)
+	if err != nil {
+		log.Fatalf("файл конфигурации '%s' недоступен для чтения: %v", filename, err)
+		return config
+	}
+
+	// Read config file
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("ошибка чтения файла '%s': %v", filename, err)
+		return config
+	}
+
+	// Parsing YAML to struct
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("ошибка парсинга файла '%s': %v", filename, err)
+		return config
+	}
+
+	return config
+}
